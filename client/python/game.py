236c5eeaf90c27eca416cdf43f76d6d1843f3985
Zhiyao (John) Ma <majohn@mit.edu>
2014-10-25 18:43:22 -0400

    fixes

--- client/python/game.py
+++ client/python/game.py
@@ -68,7 +68,7 @@ def connected_squares(square, player_number):
         top_vertex = stack.pop(len(stack)-1)
         children = adjacent_to(top_vertex)
         for child in children:
-            if child not in visited and :
+            if child not in visited:
                 stack.append(child)
         visited.append(top_vertex)
     return visited
@@ -135,16 +135,13 @@ def heuristic1(grid, bonus_squares, player_number):
     for i in range(40):
         for j in range(40):
             if grid[i][j] == player_number:
-                if all([isEmpty(i+deltaI, j+deltaJ) for deltaI, deltaJ in [(1,0),(-1,0),(0,1),(0,-1)]])
-                    if any([isEmpty(i+deltaI, j+deltaJ) for deltaI, deltaJ in [(1,0),(-1,0),(0,1),(0,-1)]])
+                if all([not isFilled(i+deltaI, j+deltaJ, player_number) for deltaI, deltaJ in [(1,0),(-1,0),(0,1),(0,-1)]]):
+                    if any([isFilled(i+deltaI, j+deltaJ, player_number) for deltaI, deltaJ in [(1,1),(-1,1),(1,-1),(-1,-1)]]):
                         num_corners += 1
 
     
     return 0.1 * current_score + num_corners
     
-def isEmpty(i, j, grid):
-    return (i >= 0 and i < 20 and j >= 0 and j < 20 and grid[i][j] == -1)
-
 def isFilled(i, j, grid, player_number):
     return (i >= 0 and i < 20 and j >= 0 and j < 20 and grid[i][j] == player_number)
 
